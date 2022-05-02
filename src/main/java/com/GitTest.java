c41cbb230d465d1a1f4004447f48cf09d4d8d0fe
sanmu <22484@qq.com>
2022-05-02 13:20:34 +0800

    hot-test2 commit

--- src/main/java/com/GitTest.java
+++ src/main/java/com/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello 2b!");
         System.out.println("zhelishihot-fix2222~");
+        System.out.println("push");
     }
 }
